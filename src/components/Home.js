1c09c01edcfdac587004367c383088527a5d07ef
LB45000 <144392042+LB45000@users.noreply.github.com>
2024-03-27 13:56:22 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
       <h1>Welcome to my Website</h1>
-      <p>I'm a passionate developer dedicated to building products that have a real world impact.</p>
+      <p>My name is Luis Bravo and I'm a passionate developer dedicated to building products that have a real world impact.</p>
       <div style={{ marginTop: '40px' }}>
         <h2>Connect With Me</h2>
         <a href="https://www.instagram.com/" style={{ marginRight: '10px' }}>Instagram</a>
@@ -16,3 +16,4 @@ function Home() {
 }
 
 export default Home;
+
